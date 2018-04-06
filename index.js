bebaa25aee7af41349d5e3e83fb43682382f3362
Frank Warnaar <frank@voorhoede.nl>
2018-04-06 11:55:01 +0200

    feat(camera): fetch file with POST to example endpoint

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const input = document.querySelector('[data-input]');
 const preview = document.querySelector('[data-preview]');
+const submitButton = document.querySelector('[data-]')
 
 input.addEventListener('change', saveImage);
 
 function saveImage() {
   if (input.files && input.files[0]) {
     const reader = new FileReader();
+    const file = input.files[0];
 
     reader.onload = event => {
       preview.setAttribute('src', event.target.result);
     }
 
-    reader.readAsDataURL(input.files[0]);
+    reader.readAsDataURL(file);
+    fetch('http://www.example.net', {
+      method: 'POST',
+      body: file,
+    }).then(response => response.json())
   }
 }
